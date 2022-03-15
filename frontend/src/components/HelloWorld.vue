4fc689801b3b1c21c7f75a0d06fd10ef5c8bf14a
Quan To <btquanto@gmail.com>
2022-03-15 10:46:17 +0700

    Integrate project with tailwindcss

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="bg-red-50">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -31,6 +31,8 @@
 </template>
 
 <script>
+import "@/assets/css/tailwind.css";
+
 export default {
   name: "HelloWorld",
   props: {
---
+++ tailwind.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./frontend/src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

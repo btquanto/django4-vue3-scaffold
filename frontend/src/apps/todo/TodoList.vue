34a5af69ce2e1367f3e76fe605b50a98e4eef9d5
Quan To <btquanto@gmail.com>
2022-03-25 16:11:48 +0700

    Add remove task button. Add button to navigate to AddTodoItem screen

--- frontend/src/apps/todo/TodoList.vue
+++ frontend/src/apps/todo/TodoList.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <table class="$border-separate $w-full">
+  <div class="$flex $flex-col">
+    <table class="$border-separate">
+      <colgroup>
+        <col class="$w-10" />
+        <col class="$w-64" />
+        <col class="" />
+        <col class="$w-40" />
+        <col class="$w-40" />
+      </colgroup>
       <thead class="$text-grey-700 $bg-gray-100 $uppercase">
         <tr>
-          <th scope="col" class="$border $px-6 $py-3">No</th>
-          <th scope="col" class="$border $px-6 $py-3">Task Name</th>
-          <th scope="col" class="$border $px-6 $py-3">Description</th>
-          <th scope="col" class="$border $px-6 $py-3">Status</th>
+          <th scope="col" class="$border $px-6 $py-3"></th>
+          <th scope="col" class="$border $px-6 $py-3">{{ tt("Task Name") }}</th>
+          <th scope="col" class="$border $px-6 $py-3">{{ tt("Description") }}</th>
+          <th scope="col" class="$border $px-6 $py-3">{{ tt("Status") }}</th>
+          <th scope="col" class="$border $px-6 $py-3">{{ tt("Action") }}</th>
         </tr>
       </thead>
       <tbody>
@@ -15,14 +23,23 @@
           <td class="$border $px-6 $py-3 $text-left">{{ item.name }}</td>
           <td class="$border $px-6 $py-3 $text-left">{{ item.description }}</td>
           <td class="$border $px-6 $py-3 $text-center">{{ item.status }}</td>
+          <td class="$border $px-6 $py-3 $text-center">
+            <button :title="tt('Remove')" @click="$store.dispatch('removeTodoItem', item)"><TrashIcon class="$h-6 $w-6" /></button>
+          </td>
         </tr>
       </tbody>
     </table>
+  </div>
+  <div class="$mt-4 $w-full $flex $flex-row $justify-end">
+    <button class="$flex $flex-row $items-center $px-3 $py-1 $bg-cyan-600 hover:$bg-cyan-700 $rounded-full $text-white $w-max" @click="$router.push('/add')">
+      <PlusIcon class="$h-4 $w-4" /> <span class="$ml-1">New</span>
+    </button>
   </div>
 </template>
 <script setup>
 import "@/assets/css/tailwind.css";
 import { mapGetters } from "vuex";
+import { TrashIcon, PlusIcon } from "@heroicons/vue/solid";
 </script>
 <script>
 export default {

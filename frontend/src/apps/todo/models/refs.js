c5cd2cd28d26689da403bf1fcf0bd23c41703f55
Quan To <btquanto@gmail.com>
2022-07-15 16:21:26 +0700

    Fix: errors rendering front-end

--- frontend/src/apps/todo/models/refs.js
+++ frontend/src/apps/todo/models/refs.js
@@ -2,7 +2,7 @@ import { ref } from "vue";
 import { TodoItemPriority, TodoItemStatus } from "./todo";
 
 export const TodoItemPriorities = ref(Object.fromEntries(Object.entries(TodoItemPriority)));
-export const PriorityText = ref(TodoItemPriority.getText);
+export const PriorityText = ref(TodoItemPriority.$getText);
 
 export const TodoItemStatuses = ref(Object.fromEntries(Object.entries(TodoItemStatus)));
-export const StatusText = ref(TodoItemStatus.getText);
+export const StatusText = ref(TodoItemStatus.$getText);
--- frontend/src/utils/i18n.js
+++ frontend/src/utils/i18n.js
@@ -6,8 +6,16 @@ export const LANGUAGES = {
   ja: "Japanese",
 };
 
-export default createGettext({
+const gettext = createGettext({
   availableLanguages: LANGUAGES,
   defaultLanguage: "en",
   translations: translations,
 });
+
+export const $gettext = gettext.$gettext;
+export const $pgettext = gettext.$pgettext;
+export const $ngettext = gettext.$ngettext;
+export const $npgettext = gettext.$npgettext;
+export const $interpolate = gettext.$interpolate;
+
+export default gettext;

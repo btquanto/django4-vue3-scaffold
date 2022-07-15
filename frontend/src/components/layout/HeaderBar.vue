ee5657cd9fb05d1fe89664ddb3fbd91f25283724
Quan To <btquanto@gmail.com>
2022-07-15 17:32:42 +0700

    Increase clickbox for Header Menu Icon

--- frontend/src/components/layout/HeaderBar.vue
+++ frontend/src/components/layout/HeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="$bg-white $shadow $px-1 $py-4">
-    <button class="$text-cyan-600" @click="$store.dispatch('toggleSidebar')">
+  <header class="$bg-white $shadow $px-1 $py-2">
+    <button class="$p-2 $text-cyan-600" @click="$store.dispatch('toggleSidebar')">
       <MenuIcon class="$h-6 $w-6" />
     </button>
   </header>
